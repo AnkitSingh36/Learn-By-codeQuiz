e551c488aa48326ec27cd58bfa80f0c5a4dd31fd
AnkitSingh36 <ankit.singh3696@gmail.com>
2021-11-02 19:25:25 +0530

    Updates

---
+++ static/js/21.640b738e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplearn-by-quiz"]=this["webpackJsonplearn-by-quiz"]||[]).push([[21],{273:function(e,n,t){"use strict";t.r(n),t.d(n,"amplify_container",(function(){return r}));var i=t(19),r=function(){function e(e){Object(i.k)(this,e)}return e.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},e}();r.style="amplify-container{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=21.640b738e.chunk.js.map
